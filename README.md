cfabd85cdd31173d603e665dbf017805a11dfd1b
Aleksbgbg <kozichka01@abv.bg>
2018-09-27 20:29:28 +0100

    Initialise repository.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple RTF
+Simple RTF library for C# applications.
---
+++ SimpleRtf/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleRtf")]
+[assembly: AssemblyDescription("Simple RTF library for C# applications.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleRtf")]
+[assembly: AssemblyCopyright("Copyright © Aleks Todorov 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5cc5bc65-e817-4ce3-a7f5-78f31ecab5a7")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
