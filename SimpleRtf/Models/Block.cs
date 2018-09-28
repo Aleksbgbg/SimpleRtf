998eeab7a567ef9b5eade2e464b52de454670341
Aleksbgbg <kozichka01@abv.bg>
2018-09-28 08:55:23 +0100

    [Block] Add base class for paragraph nodes which includes a list of inlines.

---
+++ SimpleRtf/Models/Block.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleRtf.Models
+{
+    using System.Collections.Generic;
+
+    public abstract class Block
+    {
+        private protected List<Inline> Inlines { get; } = new List<Inline>();
+    }
+}
