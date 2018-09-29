ab350d2c1f0f5b5eef9e91bd250878780df3297e
Aleksbgbg <kozichka01@abv.bg>
2018-09-29 14:05:54 +0100

    [Content] Add FormatRtf overload accepting a StringBuilder (returning void). First overload calls second.

--- SimpleRtf/Models/Content.cs
+++ SimpleRtf/Models/Content.cs
@@ -1,11 +1,22 @@
 ﻿namespace SimpleRtf.Models
 {
+    using System.Text;
+
     public abstract class Content
     {
         public string Font { get; set; } = "Calibri (Body)";
 
         public double FontSize { get; set; } = 12.0;
 
-        internal abstract string FormatRtf();
+        internal string FormatRtf()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            FormatRtf(stringBuilder);
+
+            return stringBuilder.ToString();
+        }
+
+        internal abstract void FormatRtf(StringBuilder stringBuilder);
     }
 }
