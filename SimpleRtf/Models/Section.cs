e6a0c3991b863d353b602263217c6a476696fd81
Aleksbgbg <kozichka01@abv.bg>
2018-09-29 14:27:30 +0100

    [Section] Add class which contains mutliple blocks (paragraphs).

---
+++ SimpleRtf/Models/Section.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleRtf.Models
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Section : Content
+    {
+        public List<Block> Blocks { get; } = new List<Block>();
+
+        internal override void FormatRtf(StringBuilder stringBuilder)
+        {
+            foreach (Block block in Blocks)
+            {
+                block.FormatRtf(stringBuilder);
+            }
+        }
+    }
+}
