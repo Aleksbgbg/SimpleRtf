8346ff1379c20c37cf07e146ed102f0a2b578cb1
Aleksbgbg <kozichka01@abv.bg>
2018-09-29 14:32:20 +0100

    [Block] Add FormatRtf override.

--- SimpleRtf/Models/Block.cs
+++ SimpleRtf/Models/Block.cs
@@ -1,9 +1,18 @@
 ﻿namespace SimpleRtf.Models
 {
     using System.Collections.Generic;
+    using System.Text;
 
     public abstract class Block : Content
     {
         public List<Inline> Inlines { get; } = new List<Inline>();
+
+        internal override void FormatRtf(StringBuilder stringBuilder)
+        {
+            foreach (Inline inline in Inlines)
+            {
+                inline.FormatRtf(stringBuilder);
+            }
+        }
     }
 }
