9cb76cfe64ad35424fa1c1e22693d57b0ca93515
Aleksbgbg <kozichka01@abv.bg>
2018-09-29 14:37:37 +0100

    [Document] Add FormatRtf override, and SaveAsync method. Convert Blocks to Sections.

--- SimpleRtf/Models/Document.cs
+++ SimpleRtf/Models/Document.cs
@@ -1,9 +1,40 @@
 ﻿namespace SimpleRtf.Models
 {
     using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using SimpleRtf.Constants;
 
     public sealed class Document : Content
     {
-        private readonly List<Block> _blocks = new List<Block>();
+        public List<Section> Sections { get; } = new List<Section>();
+
+        public async Task SaveAsync(Stream stream)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(stream, Encoding.ASCII, 1024, true))
+            {
+                await streamWriter.WriteAsync(FormatRtf());
+            }
+
+            stream.Position = 0;
+        }
+
+        internal override void FormatRtf(StringBuilder stringBuilder)
+        {
+            stringBuilder.Append('{');
+
+            // Header
+            stringBuilder.AppendFormat(@"\rtf1{0}", CharacterSet.Ansi);
+
+            // Document
+            foreach (Section section in Sections)
+            {
+                section.FormatRtf(stringBuilder);
+            }
+
+            stringBuilder.Append('}');
+        }
     }
 }
