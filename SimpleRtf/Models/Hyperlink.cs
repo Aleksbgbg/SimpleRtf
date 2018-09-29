b1c2fdc3f49f5fa6e88c7b0c03da9dc30f722faf
Aleksbgbg <kozichka01@abv.bg>
2018-09-29 14:20:53 +0100

    [Hyperlink] Add hyperlink inline, with URL and DisplayText.

---
+++ SimpleRtf/Models/Hyperlink.cs
@@ -0,0 +1,27 @@
+﻿namespace SimpleRtf.Models
+{
+    using System.Text;
+
+    public class Hyperlink : Inline
+    {
+        public Hyperlink(string url)
+        {
+            Url = url;
+        }
+
+        public Hyperlink(string url, string displayText)
+        {
+            Url = url;
+            DisplayText = displayText;
+        }
+
+        public string Url { get; }
+
+        public string DisplayText { get; set; }
+
+        internal override void FormatRtf(StringBuilder stringBuilder)
+        {
+            stringBuilder.AppendFormat(@"{{\field{{\*\fldinst HYPERLINK ""{0}""}}{{\fldrslt {1}}}", Url, DisplayText);
+        }
+    }
+}
