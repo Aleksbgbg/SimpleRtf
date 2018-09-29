25a58e9063d406924b7c42d067dc1d1f84c419cc
Aleksbgbg <kozichka01@abv.bg>
2018-09-29 14:08:27 +0100

    [CharacterSet] Add class with all RTF character sets.

---
+++ SimpleRtf/Constants/CharacterSet.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleRtf.Constants
+{
+    internal static class CharacterSet
+    {
+        internal const string Ansi = @"\ansi";
+
+        internal const string Mac = @"\mac";
+
+        internal const string Pc = @"\pc";
+
+        internal const string Pca = @"\pca";
+    }
+}
